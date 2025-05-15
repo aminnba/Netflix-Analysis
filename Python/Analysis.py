0e71a2e4553445368e177fa7d7cdf4bb58a0863c
aminnba <aminn.baaza@gmail.com>
2025-05-15 01:57:54 +0100

    Delete Python/Analysis.py

--- Python/Analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import sqlite3
-
-conn = sqlite3.connect(
-    'movies.db')  # Assuming you have the same database setup
-movies_df = pd.read_sql_query("SELECT * FROM movies", conn)
-conn.close()
-
